9989ca7e5ea991de0497b8fa52f81b59954d4c06
Christos <giachoud@gapps.auth.gr>
2023-01-07 23:16:15 +0200

    Working

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,22 @@
 import express from "express";
+import mysql from "mysql2";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
+    // make database connection
+    const eco = mysql.createConnection({
+
+        host: "localhost",
+        user: "admin",
+        password: process.env.admin_pass,
+        database: "ecoshelterdb",
+
+    });
+    eco.query("SELECT * FROM protected_animal", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+      });
     return res.send("Hello from admin");
 });
 
